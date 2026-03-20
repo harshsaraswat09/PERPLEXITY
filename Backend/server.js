bee9bfdea5325659f4652c61dc3b9383412bb384
Harsh Saraswat <harshsaraswat09@gmail.com>
2026-03-21 04:40:09 +0530

    integrate Socket.IO for real-time communication and implement chat functionality

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,17 @@
 import "dotenv/config"
 import app from "./src/app.js"
+import http from "http"
 import connectToDB from "./src/config/database.js"
+import { initSocket } from "./src/sockets/server.socket.js"
 
 
+const httpServer = http.createServer(app)
+
+initSocket(httpServer)
 
 connectToDB()
 
 
-app.listen(3000, ()=>{
+httpServer.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 })
